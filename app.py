e420cde2f60cbfc5081499c6db8585400d8721fb
Rakesh kumar <rakeshkumar93694@gmail.com>
2025-04-05 12:58:52 +0530

    add test logic

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ def sub(a, b):
     return a - b
 
 
-result1 = add(4, 5)
-result2 = sub(4, 1)
+if __name__ == "__main__":
+    result1 = add(4, 5)
+    result2 = sub(4, 1)
 
-print(result1, result2)
+    print(result1, result2)
